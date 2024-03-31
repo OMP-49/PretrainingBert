6dcab83072233e5a2bc878d3df0fee769be9c644
OMP-49 <om@Oms-MacBook-Air.local>
2024-03-31 19:33:59 -0400

    Passing config values as arguments

--- pretraining.py
+++ pretraining.py
@@ -1,7 +1,5 @@
 import json
 from pathlib import Path
-from typing import Iterator
-import time
 import torch
 from transformers import (
     BertTokenizerFast,
@@ -22,57 +20,47 @@ import argparse
 os.environ["WANDB_DISABLED"] = "true"
 
 #Constants
-
-LIMIT_DATASET = 2016  # keep small for development, set to None for full dataset
-
 RANDOM_SEED = 42
-NUM_TOKENIZER_TRAINING_ITEMS = 1_000_000  # I made this up, but it seems reasonable
+NUM_TOKENIZER_TRAINING_ITEMS = 1_000_000
 VOCAB_SIZE = 32_768 
-DEVICE_BATCH_SIZE = 512 # adjust to get near 100% gpu memory use
-MODEL_MAX_SEQ_LEN = 96  
 HIDDEN_SIZE = 128
 VERBOSE = True
-WIKI = True
-BOOK = False
-gradient_accumulation_steps = 2048 // DEVICE_BATCH_SIZE 
-batch_size = DEVICE_BATCH_SIZE * gradient_accumulation_steps
-print(f"{DEVICE_BATCH_SIZE = }")
-print(f"{gradient_accumulation_steps = }")
-print(f"{batch_size = }")
-
-RUN_DIR = Path("data") / f"run_{time.strftime('%Y%m%d-%H%M%S')}"
-# CHECKPOINT_DIR = RUN_DIR / "training_checkpoints"
-MODEL_DIR = RUN_DIR / "model"
-TOKENIZER_PATH = RUN_DIR / "tokenizer.json"
-TRAINER_HISTORY_PATH = RUN_DIR / "trainer_history.json"
-
-RUN_DIR.mkdir(exist_ok=True, parents=True)
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-    # # training
-    # parser.add_argument("max_seq_length", type=int, help="Max sequence length to use. Affects the \
-    #                     input/output dimension of the MLP.")
-    # parser.add_argument("--batch_size", type=int, default=32)
-    # parser.add_argument("--lr", default=5e-5, type=float)
-    # parser.add_argument('--epoch', default=1, type=int, help="Number of pass through the dataset")
+    # model
+    parser.add_argument("max_seq_length", type=int, help="Max sequence length to use. Affects the \
+                        input/output dimension of the MLP.")
 
-    # # datasets
-    # parser.add_argument("--wiki", action='store_true', help="Whether to use wiki dataset")
-    # parser.add_argument("--book", action="store_true", help="Whether to use bookcorpus dataset")
-    # parser.add_argument("--n_examples", type=int, default=None, help="Number of examples from dataset used. \
-    #                     Mainly for specifying a small number for code testing. Default is None, and uses \
-    #                     the entire dataset.")
+    # training
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--lr", default=5e-5, type=float)
+    parser.add_argument('--epoch', default=1, type=int, help="Number of pass through the dataset")
+
+    # datasets
+    parser.add_argument("--wiki", action='store_true', help="Whether to use wiki dataset")
+    parser.add_argument("--book", action="store_true", help="Whether to use bookcorpus dataset")
+    parser.add_argument("--n_examples", type=int, default=None, help="Number of examples from dataset used. \
+                        Mainly for specifying a small number for code testing. Default is None, and uses \
+                        the entire dataset.")
+    parser.add_argumnet('--offset',default = 0, type=int, help = "Offset dataset")
     # parser.add_argument("--test_train_split", type = float, default= 0.1)
     
-    # # experiment name for saving checkpoints, trained model, config, etc.
-    # parser.add_argument('--exp_name', default='test', type=str)
+    # experiment name for saving checkpoints, trained model, config, etc.
+    parser.add_argument('--exp_name', default='test', type=str)
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
+
+    RUN_DIR = Path("experiments") / f"{args.exp_name}"
+    MODEL_DIR = RUN_DIR / "model"
+    TOKENIZER_PATH = RUN_DIR / "tokenizer.json"
+    TRAINER_HISTORY_PATH = RUN_DIR / "trainer_history.json"
+
+    RUN_DIR.mkdir(exist_ok=True, parents=True)
 
     with MagicTimer() as timer:
-        dataset = create_dataset(WIKI, BOOK, LIMIT_DATASET, VERBOSE)
+        dataset = create_dataset(args.wiki, args.book, args.n_examples, VERBOSE)
         print(f"Loaded dataset in {timer}")
     
     #split data into train and validation sets
@@ -82,21 +70,21 @@ if __name__ == "__main__":
     create_tokenizer(dataset["train"], VOCAB_SIZE, str(TOKENIZER_PATH))
     tokenizer = BertTokenizerFast(tokenizer_file=str(TOKENIZER_PATH))
 
-    tokenized_dataset_train = offset_dataset(dataset["train"], tokenizer,MODEL_MAX_SEQ_LEN, verbose=False, stride= MODEL_MAX_SEQ_LEN - 1)
-    tokenized_dataset_eval = offset_dataset(dataset["test"], tokenizer,MODEL_MAX_SEQ_LEN, verbose=False)
+    tokenized_dataset_train = offset_dataset(dataset["train"], tokenizer,args.max_seq_length, verbose=False, stride= args.max_seq_length - args.offset)
+    tokenized_dataset_eval = offset_dataset(dataset["test"], tokenizer, verbose=False)
 
     #load BERT model
     model_config = BertConfig(
         vocab_size=VOCAB_SIZE,
         hidden_size=HIDDEN_SIZE,
-        max_sequence_length = MODEL_MAX_SEQ_LEN,
+        max_sequence_length = args.max_seq_length,
         num_hidden_layers = 2,
         num_attention_heads = 2,
         intermediate_size=4*HIDDEN_SIZE,
         max_position_embeddings=HIDDEN_SIZE,
 
     )
-    model = BertForMaskedLM(model_config, max_sequence_length=MODEL_MAX_SEQ_LEN, mlp_layers=[1])
+    model = BertForMaskedLM(model_config, max_sequence_length=args.max_seq_length, mlp_layers=[1])
 
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer,
@@ -106,23 +94,24 @@ if __name__ == "__main__":
     )
 
     #trainer
+    gradient_accumulation_steps = 2048 // args.batch_size
+    batch_size = args.batch_size * gradient_accumulation_steps
     training_args = TrainingArguments(
-        learning_rate=1e-3,
+        learning_rate=args.lr,
         warmup_ratio=0.5,
         adam_beta1=0.9,
         adam_beta2=0.98,
         adam_epsilon=1e-9,
         weight_decay=0.01,
         max_grad_norm=0.5,
-        num_train_epochs=1,
-        per_device_train_batch_size=DEVICE_BATCH_SIZE,
+        num_train_epochs=args.epoch,
+        per_device_train_batch_size=args.batch_size,
         gradient_accumulation_steps=gradient_accumulation_steps,
         dataloader_num_workers=2,
         save_strategy = "no",
-        evaluation_strategy="steps",
+        # evaluation_strategy="steps",
         # eval_steps=10,
         logging_steps=1,
-        # output_dir=CHECKPOINT_DIR,
         optim="adamw_torch",
         report_to=None,
     )
